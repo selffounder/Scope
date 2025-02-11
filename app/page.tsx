28037c6af0061253c5d81ff33b110155e9856286
selffounder <alisher.self.founder@gmail.com>
2025-02-11 22:50:18 +0500

    first commit InshAllah!

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import {Header} from "@/components/header";
-import {Content1} from "@/components/content1"
+import {Header} from "@/components/header_content";
+import {Content1} from "@/components/landing_content"
 export default function Home() {
   return <>
   <Header />
